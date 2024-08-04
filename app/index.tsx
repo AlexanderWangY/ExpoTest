9101e82a75642190d2cda93e9e43110f06296cbf
AlexanderWangY <alexander.yisu.wang@outlook.com>
2024-08-04 11:12:09 +0800

    weird msg

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, TouchableOpacity, View } from "react-native";
+
+const Home = () => {
+  return (
+    <View className="h-full w-full bg-red-500 justify-center items-center">
+      <Text className="text-3xl text-white">HELLO</Text>
+      <TouchableOpacity className="bg-purple-500 pl-10 pr-10 pt-5 pb-5">
+        <Text>HI!</Text>
+      </TouchableOpacity>
+      <Text>WHAT</Text>
+    </View>
+  );
+};
+
+export default Home;
